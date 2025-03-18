8e124e35fafde068f5730cf25c4d898ed8ed901d
Tahani50 <tahanyayman4@gmail.com>
2025-03-18 11:53:30 +0300

    Initial Commit

---
+++ Class_Task4/Class_Task4App.swift
@@ -0,0 +1,17 @@
+//
+//  Class_Task4App.swift
+//  Class_Task4
+//
+//  Created by Taibah Valley Academy on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Class_Task4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Class_Task4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Class_Task4
+//
+//  Created by Taibah Valley Academy on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
